0074da58cc41f7334af222f4b6929ea507137c6f
Cameron Jenkinson <camjenkinson@gmail.com>
2020-02-04 11:15:48 +0000

    Add client credentials grant

--- src/grant-types/base-grant-type.js
+++ src/grant-types/base-grant-type.js
@@ -1,10 +1,80 @@
+const {
+  InvalidArgumentError,
+} = require('../utils/errors');
 
+const {
+  generateRandomToken
+} = require('../utils/generate-token');
 
-const createBaseGrantTypeHelpers = () => {
+const createBaseGrantTypeHelpers = (options = {}) => {
+  if (!options.accessTokenLifetime) {
+    throw new InvalidArgumentError('Missing parameter: `accessTokenLifetime`');
+  }
 
+  if (!options.model) {
+    throw new InvalidArgumentError('Missing parameter: `model`');
+  }
+
+  const baseGrantOptions = {
+    accessTokenLifetime: options.accessTokenLifetime,
+    model: options.model,
+    refreshTokenLifetime: options.refreshTokenLifetime,
+    alwaysIssueNewRefreshToken:options.alwaysIssueNewRefreshToken,
+  }
+
+  const generateAccessToken = async (client, user, scope) => {
+    if (baseGrantOptions.model.generateAccessToken) {
+      const accessToken = await baseGrantOptions.model.generateAccessToken(client, user, scope);
+
+      return accessToken;
+    }
+
+    return generateRandomToken();
+  }
+
+  const generateRefreshToken = async (client, user, scope) => {
+    if (baseGrantOptions.model.generateRefreshToken) {
+      const accessToken = await baseGrantOptions.model.generateRefreshToken(client, user, scope);
+
+      return accessToken;
+    }
+
+    return generateRandomToken();
+  }
+
+  const getAccessTokenExpiresAt = () => {
+    const expires = new Date();
+
+    expires.setSeconds(expires.getSeconds() + baseGrantOptions.accessTokenLifetime);
+
+    return expires;
+  }
+
+  const getRefreshTokenExpiresAt = () => {
+    const expires = new Date();
+
+    expires.setSeconds(expires.getSeconds() + baseGrantOptions.refreshTokenLifetime);
+
+    return expires;
+  }
+
+  const getScope = () => {
+    // TODO
+    return null;
+  }
+
+  const validateScope = () => {
+    // TODO
+    return false;
+  }
 
   return {
-
+    generateAccessToken,
+    generateRefreshToken,
+    getAccessTokenExpiresAt,
+    getRefreshTokenExpiresAt,
+    getScope,
+    validateScope
   }
 }
 
--- src/grant-types/client-credentials.js
+++ src/grant-types/client-credentials.js
@@ -1,14 +1,74 @@
+const {
+  InvalidArgumentError,
+  InvalidGrantError
+} = require('../utils/errors');
+
 const createBaseGrantTypeHelpers = require('./base-grant-type');
 
-const {
+const clientCredentialGrantType = (options = {}) => {
+  const {
+    generateAccessToken,
+    getAccessTokenExpiresAt,
+    getScope,
+    validateScope
+  } = createBaseGrantTypeHelpers(options);
 
-} = createBaseGrantTypeHelpers();
+  if (!options.model) {
+    throw new InvalidArgumentError('Missing parameter: `model`');
+  }
 
-const clientCredentialGrantType = () => {
+  if (!options.model.getUserFromClient) {
+    throw new InvalidArgumentError('Invalid argument: model does not implement `getUserFromClient()`');
+  }
 
+  if (!options.model.saveToken) {
+    throw new InvalidArgumentError('Invalid argument: model does not implement `saveToken()`');
+  }
+
+  const getUserFromClient = async (client) => {
+    const user = await options.model.getUserFromClient(client);
+
+    if (!user) {
+      throw new InvalidGrantError('Invalid grant: user credentials are invalid');
+    }
+
+    return user;
+  }
+
+  const saveToken = async (user, client, scope) => {
+    const validatedScope = validateScope(user, client, scope);
+    const accessToken = await generateAccessToken(client, user, scope);
+    const accessTokenExpiresAt = getAccessTokenExpiresAt(client, user, scope);
+
+    const token = {
+      accessToken,
+      accessTokenExpiresAt,
+      scope: validatedScope
+    }
+
+    return options.model.saveToken(token, client, user);
+  }
+
+  const handle = async (eventRequest, client) => {
+    if (!eventRequest) {
+      throw new InvalidArgumentError('Missing parameter: `eventRequest`');
+    }
+
+    if (!client) {
+      throw new InvalidArgumentError('Missing parameter: `client`');
+    }
+
+    const scope = getScope(eventRequest);
+
+    const user = await getUserFromClient(client);
+
+    return saveToken(user, client, scope);
+  }
 
   return {
-
+    getUserFromClient,
+    saveToken,
+    handle,
   };
 };
 
