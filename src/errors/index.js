de92cc7ebae69222b5110c39d1698ae1d648d0ef
Cameron Jenkinson <camjenkinson@gmail.com>
2020-02-05 13:17:52 +0000

    Add errors

---
+++ src/errors/index.js
@@ -0,0 +1,134 @@
+class OAuthError extends Error {
+  constructor(message, originalError, context) {
+    super(message);
+    this.expose = true;
+    this.name = this.constructor.name;
+    this.code = this.status = this.statusCode;
+    this.originalError = originalError;
+    this.context = context;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class AccessDeniedError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'access_denied';
+    this.code = 400;
+  }
+}
+
+class InsufficientScopeError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'insufficient_scope';
+    this.code = 403;
+  }
+}
+
+class InvalidArgumentError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'invalid_argument';
+    this.code = 500;
+  }
+}
+
+class InvalidClientError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'invalid_client';
+    this.code = 400;
+  }
+}
+
+class InavlidGrantError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'invalid_grant';
+    this.code = 400;
+  }
+}
+
+class InvalidRequestError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'invalid_request';
+    this.code = 400;
+  }
+}
+
+class InvalidScopeError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'invalid_scope';
+    this.code = 400;
+  }
+}
+
+class InvalidTokenError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'invalid_token';
+    this.code = 401;
+  }
+}
+
+class UnauthorizedClientError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'unauthorized_client';
+    this.code = 400;
+  }
+}
+
+class UnauthorizedRequestError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'unauthorized_request';
+    this.code = 401;
+  }
+}
+
+class UnsupportedGrantTypeError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'unsupported_grant_type';
+    this.code = 400;
+  }
+}
+
+class UnsupportedResponseTypeError extends OAuthError {
+  constructor(message, originalError) {
+    super(message, originalError);
+    this.name = 'unsupported_response_type';
+    this.code = 400;
+  }
+}
+
+class ServerError extends OAuthError {
+  constructor(message, context) {
+    super(message);
+    this.name = 'server_error';
+    this.code = 503;
+    this.context = context;
+  }
+}
+
+module.exports = {
+  OAuthError,
+  AccessDeniedError,
+  InsufficientScopeError,
+  InvalidArgumentError,
+  InvalidClientError,
+  InavlidGrantError,
+  InvalidRequestError,
+  InvalidScopeError,
+  InvalidTokenError,
+  UnauthorizedClientError,
+  UnauthorizedRequestError,
+  UnsupportedGrantTypeError,
+  UnsupportedResponseTypeError,
+  ServerError,
+}
