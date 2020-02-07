ebce68ca1278b08a0053c431732fe595f6ca2b9a
Cameron Jenkinson <camjenkinson@gmail.com>
2020-02-07 16:39:13 +0100

    Remove content type check from event parser

--- src/utils/parse-aws-event.js
+++ src/utils/parse-aws-event.js
@@ -14,11 +14,6 @@ module.exports = ({
     throw new InvalidRequestError('method must be POST');
   }
 
-  // TODO
-  // if (!headers.is('application/x-www-form-urlencoded')) {
-  //   throw new InvalidRequestError('content must be application/x-www-form-urlencoded');
-  // }
-
   return {
     headers,
     method,
